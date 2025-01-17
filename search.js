45d5d3cdec4767904cd6870a8aa4fd1b857d8b29
Manuel Lomaglio <manuelomaglio@gmail.com>
2025-01-17 05:43:11 +0100

    secondo esercizio

---
+++ search.js
@@ -0,0 +1,7 @@
+//const primaRicerca = document.querySelectorAll(".paragrafo");
+
+console.log(document.querySelectorAll(".paragrafo"));
+
+console.log(document.querySelector("#elemento-3").innerText);
+
+console.log(document.querySelectorAll("h2"));
